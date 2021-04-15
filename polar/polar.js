d58a30fd1c6ef6a1df14c8ce16aa9c11dd93a4df
Erik Fredericks <frederer@gvsu.edu>
2021-04-15 13:44:36 -0400

    updates

--- polar/polar.js
+++ polar/polar.js
@@ -1,20 +1,32 @@
 // Just a whole buncha lines drawn around a circle
 
+// Just a whole buncha lines drawn around a circle
+
+// thanks mozilla i always forget this one:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
 let points = [];
 let num_lines = 1000;
 
 let theta_vel;
 let current_index = 0;
 let r;
+let restart;
 
-function setup() {
-  createCanvas(640, 480);
+function mousePressed() {
+  restart = true;
+}
+
+function restartDrawing() {
   background(0);
+  current_index = 0;
+  points = [];
   
-  translate(width / 2, height / 2);
-  r = height * 0.45;
-  
-  stroke(155);
   // create points to draw lines around circle
   for (let _i = 0; _i < num_lines; _i++) {
     let _theta1 = random() * TWO_PI;
@@ -29,21 +41,43 @@ function setup() {
     points.push({'x1':_x1, 'y1':_y1,'x2':_x2,'y2':_y2});
     
     // Test without animation
+    //stroke(155);
     //line(_x1, _y1, _x2, _y2);
   }
+  
+  restart = false;
+}
+
+function setup() {
+  createCanvas(640, 480);
+  restart = false;
+  r = height * 0.45;
+  restartDrawing();
 }
 
 function draw() {
+  fill(0)
+  noStroke();
+  rect(0,0,150,40);
+  fill(255);
+  textSize(14);
+  text(current_index + "/" + points.length, 10, 20);
+
   translate(width / 2, height / 2);
   
-  line(points[current_index]['x1'],
-       points[current_index]['y1'],
-       points[current_index]['x2'],
-       points[current_index]['y2']);
-    
-  current_index++;
-  if (current_index >= points.length) {
-    background(0);
-    current_index = 0;
+  if (!restart) {
+    stroke(155);
+    strokeWeight(getRandomInt(1,4));
+    line(points[current_index]['x1'],
+         points[current_index]['y1'],
+         points[current_index]['x2'],
+         points[current_index]['y2']);
+
+    current_index++;
+    if (current_index >= points.length) {
+      restartDrawing();
+    }   
+  } else {
+    restartDrawing();
   }
 }
