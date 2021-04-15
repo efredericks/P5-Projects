bab144053182a7b834cd5d559c16bd5f3116646f
Erik Fredericks <frederer@gvsu.edu>
2021-04-15 14:05:16 -0400

    added puase

--- polar/polar.js
+++ polar/polar.js
@@ -16,38 +16,43 @@ let num_lines = 1000;
 let theta_vel;
 let current_index = 0;
 let r;
+
 let restart;
+let pause;
 let random_colors = false;
 
 function keyPressed() {
   if (key == "r")
     restart = true;
+  else if (key == "p")
+    pause = !pause;
 }
 
 function restartDrawing() {
   background(0);
   current_index = 0;
   points = [];
-  
+
   // create points to draw lines around circle
   for (let _i = 0; _i < num_lines; _i++) {
     let _theta1 = random() * TWO_PI;
     let _theta2 = random() * TWO_PI;
-    
+
     let _x1 = r * cos(_theta1);
     let _x2 = r * cos(_theta2);
-    
+
     let _y1 = r * sin(_theta1);
     let _y2 = r * sin(_theta2);
-    
-    points.push({'x1':_x1, 'y1':_y1,'x2':_x2,'y2':_y2});
-    
+
+    points.push({ 'x1': _x1, 'y1': _y1, 'x2': _x2, 'y2': _y2 });
+
     // Test without animation
     //stroke(155);
     //line(_x1, _y1, _x2, _y2);
   }
-  
+
   restart = false;
+  pause = false;
 }
 
 function randomizeColors() {
@@ -61,39 +66,42 @@ function setup() {
   let _chkbox = createCheckbox("Randomize Colors?", random_colors);
   _chkbox.changed(randomizeColors);
 
+  pause = false;
   restart = false;
   r = height * 0.45;
   restartDrawing();
 }
 
 function draw() {
-  fill(0)
-  noStroke();
-  rect(0,0,150,40);
-  fill(255);
-  textSize(14);
-  text("[r to restart] - " + current_index + "/" + points.length, 10, 20);
+  if (!pause) {
+    fill(0)
+    noStroke();
+    rect(0, 0, 190, 40);
+    fill(255);
+    textSize(14);
+    text("[r]estart/[p]ause - " + current_index + "/" + points.length, 10, 20);
 
-  translate(width / 2, height / 2);
-  
-  if (!restart) {
-    let _col = color(128, 128, 128);
-    if (random_colors) 
-      _col = color(getRandomInt(0,256), getRandomInt(0,256), getRandomInt(0,256));
-    stroke(_col);
-    console.log(random_colors);
+    translate(width / 2, height / 2);
 
-    strokeWeight(getRandomInt(1,4));
-    line(points[current_index]['x1'],
-         points[current_index]['y1'],
-         points[current_index]['x2'],
-         points[current_index]['y2']);
+    if (!restart) {
+      let _col = color(128, 128, 128);
+      if (random_colors)
+        _col = color(getRandomInt(0, 256), getRandomInt(0, 256), getRandomInt(0, 256));
+      stroke(_col);
+      console.log(random_colors);
 
-    current_index++;
-    if (current_index >= points.length) {
+      strokeWeight(getRandomInt(1, 4));
+      line(points[current_index]['x1'],
+        points[current_index]['y1'],
+        points[current_index]['x2'],
+        points[current_index]['y2']);
+
+      current_index++;
+      if (current_index >= points.length) {
+        restartDrawing();
+      }
+    } else {
       restartDrawing();
-    }   
-  } else {
-    restartDrawing();
+    }
   }
 }
